401d43abbba2e60b0bbd0078d059c2b8164d5329
Sachin R Doddaguni <rdsachin199@gmail.com>
2023-10-19 21:19:14 -0400

    Resolving conflicts

--- backend/test_app.py
+++ backend/test_app.py
@@ -31,11 +31,7 @@ def client():
         password = info["password"]
         app.config["MONGODB_SETTINGS"] = {
             "db": "appTracker",
-<<<<<<< Updated upstream
             "host": f"mongodb+srv://{username}:{password}@applicationtracker.287am.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-=======
-            "host": f"mongodb+srv://se510-2023:test@cluster0.n7qpg99.mongodb.net/appTracker?retryWrites=true&w=majority",
->>>>>>> Stashed changes
         }
     db = MongoEngine()
     db.disconnect()
@@ -123,148 +119,148 @@ def test_get_data(client, user):
     assert json.loads(rv.data) == [application]
 
 
-# # 4. testing if the application is saving data in database properly
-# def test_add_application(client, mocker, user):
-#     """
-#     Tests that using the application POST endpoint saves data
+# 4. testing if the application is saving data in database properly
+def test_add_application(client, mocker, user):
+    """
+    Tests that using the application POST endpoint saves data
 
-#     :param client: mongodb client
-#     :param user: the test user object
-#     """
-#     mocker.patch(
-#         # Dataset is in slow.py, but imported to main.py
-#         "app.get_new_user_id",
-#         return_value=-1,
-#     )
-#     user, header = user
-#     user["applications"] = []
-#     user.save()
-#     # mocker.patch(
-#     #     # Dataset is in slow.py, but imported to main.py
-#     #     'app.Users.save'
-#     # )
-#     rv = client.post(
-#         "/applications",
-#         headers=header,
-#         json={
-#             "application": {
-#                 "jobTitle": "fakeJob12345",
-#                 "companyName": "fakeCompany",
-#                 "date": str(datetime.date(2021, 9, 23)),
-#                 "status": "1",
-#             }
-#         },
-#     )
-#     assert rv.status_code == 200
-#     jdata = json.loads(rv.data.decode("utf-8"))["jobTitle"]
-#     assert jdata == "fakeJob12345"
-
-
-# # 5. testing if the application is updating data in database properly
-# def test_update_application(client, user):
-#     """
-#     Tests that using the application PUT endpoint functions
-
-#     :param client: mongodb client
-#     :param user: the test user object
-#     """
-#     user, auth = user
-#     application = {
-#         "id": 3,
-#         "jobTitle": "test_edit",
-#         "companyName": "test_edit",
-#         "date": str(datetime.date(2021, 9, 23)),
-#         "status": "1",
-#     }
-#     user["applications"] = [application]
-#     user.save()
-#     new_application = {
-#         "id": 3,
-#         "jobTitle": "fakeJob12345",
-#         "companyName": "fakeCompany",
-#         "date": str(datetime.date(2021, 9, 22)),
-#     }
-
-#     rv = client.put(
-#         "/applications/3", json={"application": new_application}, headers=auth
-#     )
-#     assert rv.status_code == 200
-#     jdata = json.loads(rv.data.decode("utf-8"))["jobTitle"]
-#     assert jdata == "fakeJob12345"
+    :param client: mongodb client
+    :param user: the test user object
+    """
+    mocker.patch(
+        # Dataset is in slow.py, but imported to main.py
+        "app.get_new_user_id",
+        return_value=-1,
+    )
+    user, header = user
+    user["applications"] = []
+    user.save()
+    # mocker.patch(
+    #     # Dataset is in slow.py, but imported to main.py
+    #     'app.Users.save'
+    # )
+    rv = client.post(
+        "/applications",
+        headers=header,
+        json={
+            "application": {
+                "jobTitle": "fakeJob12345",
+                "companyName": "fakeCompany",
+                "date": str(datetime.date(2021, 9, 23)),
+                "status": "1",
+            }
+        },
+    )
+    assert rv.status_code == 200
+    jdata = json.loads(rv.data.decode("utf-8"))["jobTitle"]
+    assert jdata == "fakeJob12345"
 
 
-# # 6. testing if the application is deleting data in database properly
-# def test_delete_application(client, user):
-#     """
-#     Tests that using the application DELETE endpoint deletes data
+# 5. testing if the application is updating data in database properly
+def test_update_application(client, user):
+    """
+    Tests that using the application PUT endpoint functions
 
-#     :param client: mongodb client
-#     :param user: the test user object
-#     """
-#     user, auth = user
+    :param client: mongodb client
+    :param user: the test user object
+    """
+    user, auth = user
+    application = {
+        "id": 3,
+        "jobTitle": "test_edit",
+        "companyName": "test_edit",
+        "date": str(datetime.date(2021, 9, 23)),
+        "status": "1",
+    }
+    user["applications"] = [application]
+    user.save()
+    new_application = {
+        "id": 3,
+        "jobTitle": "fakeJob12345",
+        "companyName": "fakeCompany",
+        "date": str(datetime.date(2021, 9, 22)),
+    }
 
-#     application = {
-#         "id": 3,
-#         "jobTitle": "fakeJob12345",
-#         "companyName": "fakeCompany",
-#         "date": str(datetime.date(2021, 9, 23)),
-#         "status": "1",
-#     }
-#     user["applications"] = [application]
-#     user.save()
-
-#     rv = client.delete("/applications/3", headers=auth)
-#     jdata = json.loads(rv.data.decode("utf-8"))["jobTitle"]
-#     assert jdata == "fakeJob12345"
-
-
-# # 8. testing if the flask app is running properly with status code
-# def test_alive_status_code(client):
-#     """
-#     Tests that / returns 200
-
-#     :param client: mongodb client
-#     """
-#     rv = client.get("/")
-#     assert rv.status_code == 200
+    rv = client.put(
+        "/applications/3", json={"application": new_application}, headers=auth
+    )
+    assert rv.status_code == 200
+    jdata = json.loads(rv.data.decode("utf-8"))["jobTitle"]
+    assert jdata == "fakeJob12345"
 
 
-# # Testing logging out does not return error
-# def test_logout(client, user):
-#     """
-#     Tests that using the logout function does not return an error
+# 6. testing if the application is deleting data in database properly
+def test_delete_application(client, user):
+    """
+    Tests that using the application DELETE endpoint deletes data
 
-#     :param client: mongodb client
-#     :param user: the test user object
-#     """
-#     user, auth = user
-#     rv = client.post("/users/logout", headers=auth)
-#     # assert no error occured
-#     assert rv.status_code == 200
+    :param client: mongodb client
+    :param user: the test user object
+    """
+    user, auth = user
+
+    application = {
+        "id": 3,
+        "jobTitle": "fakeJob12345",
+        "companyName": "fakeCompany",
+        "date": str(datetime.date(2021, 9, 23)),
+        "status": "1",
+    }
+    user["applications"] = [application]
+    user.save()
+
+    rv = client.delete("/applications/3", headers=auth)
+    jdata = json.loads(rv.data.decode("utf-8"))["jobTitle"]
+    assert jdata == "fakeJob12345"
 
 
-# def test_resume(client, mocker, user):
-#     """
-#     Tests that using the resume endpoint returns data
+# 8. testing if the flask app is running properly with status code
+def test_alive_status_code(client):
+    """
+    Tests that / returns 200
 
-#     :param client: mongodb client
-#     :param mocker: pytest mocker
-#     :param user: the test user object
-#     """
-#     mocker.patch(
-#         # Dataset is in slow.py, but imported to main.py
-#         "app.get_new_user_id",
-#         return_value=-1,
-#     )
-#     user, header = user
-#     user["applications"] = []
-#     user.save()
-#     data = dict(
-#         file=(BytesIO(b"testing resume"), "resume.txt"),
-#     )
-#     rv = client.post(
-#         "/resume", headers=header, content_type="multipart/form-data", data=data
-#     )
-#     assert rv.status_code == 200
-#     rv = client.get("/resume", headers=header)
-#     assert rv.status_code == 200
+    :param client: mongodb client
+    """
+    rv = client.get("/")
+    assert rv.status_code == 200
+
+
+# Testing logging out does not return error
+def test_logout(client, user):
+    """
+    Tests that using the logout function does not return an error
+
+    :param client: mongodb client
+    :param user: the test user object
+    """
+    user, auth = user
+    rv = client.post("/users/logout", headers=auth)
+    # assert no error occured
+    assert rv.status_code == 200
+
+
+def test_resume(client, mocker, user):
+    """
+    Tests that using the resume endpoint returns data
+
+    :param client: mongodb client
+    :param mocker: pytest mocker
+    :param user: the test user object
+    """
+    mocker.patch(
+        # Dataset is in slow.py, but imported to main.py
+        "app.get_new_user_id",
+        return_value=-1,
+    )
+    user, header = user
+    user["applications"] = []
+    user.save()
+    data = dict(
+        file=(BytesIO(b"testing resume"), "resume.txt"),
+    )
+    rv = client.post(
+        "/resume", headers=header, content_type="multipart/form-data", data=data
+    )
+    assert rv.status_code == 200
+    rv = client.get("/resume", headers=header)
+    assert rv.status_code == 200

8a13e0cb53d876e2356e801de8b8ff2ecd62dc80
amritav <amritavisalam@gmail.com>
2023-11-19 01:38:26 -0500

    Add COntact Us Page

---
+++ career-flow-ui/src/components/ContactUsPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Typography, Link} from '@mui/material';
+
+
+const ContactUsPage = (props) => {
+  return (
+    <div>
+      <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '60px' }}>
+        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            Contact Us
+                        </Typography>
+        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+          Use the email below to contact the Career Flow development team. It's a group mailbox that is accessed by the core team. Feel free to reach out to us for any questions, feedback, or inquiries related to our application.
+        </Typography>
+        <Typography variant="h6" style={{ marginTop: '20px' }}>
+          Email: <Link href="mailto:contactus_careerflow@​yahoo.com">contactus_careerflow@​yahoo.com</Link>
+        </Typography>
+        <Typography variant="h6" color="text.secondary" style={{ marginTop: '20px' }}>
+          We will respond to you as soon as we can!
+        </Typography>
+      </Container>
+    </div>
+  );
+};
+
+export default ContactUsPage;
